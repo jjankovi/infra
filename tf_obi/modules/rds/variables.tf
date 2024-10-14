0a10f3cf403f967d046c02636b41e8007d582cb8
jankovj <jaroslav.jankovic@softec.sk>
2024-10-14 09:56:16 +0200

    Try lambda function for schema creation

--- tf_obi/modules/rds/variables.tf
+++ tf_obi/modules/rds/variables.tf
@@ -1,5 +1,102 @@
 variable "enviroment" {
   description = "Environment"
+}
+
+variable "security_group_ids" {
+  type        = list(string)
+  default     = []
+  description = "The IDs of the security groups from which to allow `ingress` traffic to the DB instance"
+}
+
+variable "allowed_cidr_blocks" {
+  type        = list(string)
+  default     = []
+  description = "The whitelisted CIDRs which to allow `ingress` traffic to the DB instance"
+}
+
+variable "database_name" {
+  type        = string
+  default     = null
+  description = "The name of the database to create when the DB instance is created"
+}
+
+variable "database_user" {
+  type        = string
+  default     = null
+  description = "Username for the primary DB user."
+}
+
+variable "database_password" {
+  type        = string
+  default     = null
+  description = "Password for the primary DB user."
+}
+
+variable "database_port" {
+  type        = number
+  default = 5432
+  description = "Database port (_e.g._ `3306` for `MySQL`). Used in the DB Security Group to allow access to the DB instance from the provided `security_group_ids`"
+}
+
+variable "schemas" {
+  type = list(object({
+    schema_name    = string
+    user_name      = string
+    user_password  = string
+  }))
+  default     = []
+  description = "A list of DB schemas with owner user"
+}
+
+variable "multi_az" {
+  type        = bool
+  default     = false
+  description = "Set to true if multi AZ deployment must be supported"
+}
+
+variable "storage_type" {
+  type        = string
+  default     = "gp2"
+  description = "One of 'standard' (magnetic), 'gp2' (general purpose SSD), 'gp3' (general purpose SSD), or 'io1' (provisioned IOPS SSD)"
+}
+
+variable "storage_encrypted" {
+  type        = bool
+  default     = true
+  description = "(Optional) Specifies whether the DB instance is encrypted. The default is false if not specified"
+}
+
+variable "iops" {
+  type        = number
+  default     = null
+  description = "The amount of provisioned IOPS, only valid for certain values of storage_type."
+}
+
+variable "allocated_storage" {
+  type        = number
+  default     = 20
+  description = "The allocated storage in GBs. Required unless a `snapshot_identifier` or `replicate_source_db` is provided."
+}
+
+variable "max_allocated_storage" {
+  type        = number
+  description = "The upper limit to which RDS can automatically scale the storage in GBs"
+  default     = 0
+}
+
+variable "engine" {
+  type        = string
+  default     = "postgres"
+  description = "Database engine type (mysql, postgres, oracle-*, sqlserver-*)"
+}
+
+variable "engine_version" {
+  type        = string
+  default = "16.3"
+  description = "Database engine version, depends on engine type."
+}
+
+variable "instance_class" {
   type        = string
 }
 
--- tf_obi/stages/dev/main.tf
+++ tf_obi/stages/dev/main.tf
@@ -33,3 +33,29 @@ module "app_parameters" {
 #  admin_username    = "superuser"
 #  admin_password    = "supersecretpassword"
 #}
+
+resource "aws_secretsmanager_secret" "db_password_secret" {
+  name = "obi_db_admin_pass"
+}
+
+resource "aws_secretsmanager_secret_version" "db_password_secret_version" {
+  secret_id     = aws_secretsmanager_secret.db_password_secret.id
+  secret_string = "vqUVyzeeBoIbrMHbkrSpYafGsGodLYEqmaXDovSAmTZSeUXWeG"
+}
+
+module "rds" {
+  source            = "../../modules/rds"
+  database_name     = "obi"
+  enviroment        = "dev"
+  database_user     = "superuser"
+  database_password = aws_secretsmanager_secret_version.db_password_secret_version.secret_string
+  schemas = [
+    {
+      schema_name   = "obi_app"
+      user_name     = "obi_app"
+      user_password = "obi_pass"
+    }
+  ]
+  subnet_ids         = ["subnet-05817cc08c4c95490", "subnet-0c23826ed22f9bec4", "subnet-0446f595633fdd966"] # to su private subnety
+  security_group_ids = ["sg-0262c7593c2382f27"]                                                             # to je nat instance - len pre test
+}
